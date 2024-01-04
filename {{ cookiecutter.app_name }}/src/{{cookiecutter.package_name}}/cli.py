cb7c165e167029ed10e59a42710b29eafec8f674
Steven van de Graaf <steven@vandegraaf.xyz>
2024-01-04 11:27:08 +0100

    :sparkles: Show arguments in help with `rich_click`

--- {{ cookiecutter.app_name }}/src/{{cookiecutter.package_name}}/cli.py
+++ {{ cookiecutter.app_name }}/src/{{cookiecutter.package_name}}/cli.py
@@ -4,13 +4,18 @@ import {%- if cookiecutter.use_rich %} rich_click as {%- endif %} click
 from . import __version__
 
 context_settings = {"help_option_names": ["-h", "--help"]}
+{%- if cookiecutter.use_rich %}
+help_config = click.RichHelpConfiguration(
+    width=88,
+    show_arguments=True,
+    use_rich_markup=True,
+)
+{%- endif %}
 
 
 @click.group(context_settings=context_settings)
 {%- if cookiecutter.use_rich %}
-@click.rich_config(
-    help_config=click.RichHelpConfiguration(use_rich_markup=True, width=88)
-)
+@click.rich_config(help_config=help_config)
 {%- endif %}
 @click.version_option(__version__, "-v", "--version")
 def cli() -> None:
