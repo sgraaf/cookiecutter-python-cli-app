d3a07257375fc1d1485ef812ffc31e148bb67f3c
Steven van de Graaf <steven@vandegraaf.xyz>
2024-03-31 23:15:53 +0200

    :sparkles: Turn `CliRunner` from `click.testing` into a fixture

--- {{ cookiecutter.app_name }}/tests/test_{{cookiecutter.package_name}}.py
+++ {{ cookiecutter.app_name }}/tests/test_{{cookiecutter.package_name}}.py
@@ -1,10 +1,15 @@
 from click.testing import CliRunner
 
+import pytest
 from {{ cookiecutter.package_name }}.cli import cli
 
 
-def test_version():
-    runner = CliRunner()
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+def test_version(runner: CliRunner) -> None:
     with runner.isolated_filesystem():
         result = runner.invoke(cli, ["--version"])
         assert result.exit_code == 0
